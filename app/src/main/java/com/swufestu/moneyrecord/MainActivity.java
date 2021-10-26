5d27f52487320749f6a17457a3a04ce562b5594b
Ivy0819 <798603449@qq.com>
2021-10-26 10:55:29 +0800

    -根据点击判断记账TAG -修改记账时间

--- app/src/main/java/com/swufestu/moneyrecord/MainActivity.java
+++ app/src/main/java/com/swufestu/moneyrecord/MainActivity.java
@@ -2,25 +2,32 @@ package com.swufestu.moneyrecord;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener
 {
 
-    TextView income,outcome,cancel,finish;
+    EditText money_input;
+    TextView income,outcome,cancel,finish,record_time;
     GridView GridList_outcome,GridList_income;
     private static MyRecordAdapter adapter_income,adapter_outcome;
-    String selectTag;
+    String selectTag,time_set,record_money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         GridList_income = findViewById(R.id.GridList_income);
         outcome = findViewById(R.id.out_come);
         income = findViewById(R.id.in_come);
+        record_time = findViewById(R.id.record_time);
+        money_input = findViewById(R.id.input_money);
 //        GridList_income.setItemsCanFocus (false);
         GridList_income.setChoiceMode (ListView.CHOICE_MODE_SINGLE);
 //        GridList_outcome.setItemsCanFocus (false);
@@ -40,6 +49,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         outcome.setTextColor(getResources().getColor(R.color.gray));
         GridList_income.setVisibility(View.GONE);
         GridList_outcome.setVisibility(View.VISIBLE);
+        GridList_outcome.setContentDescription("show");
+        GridList_income.setContentDescription("hide");
+
+        // 获取当前的年、月、日、小时、分钟
+        Calendar c = Calendar.getInstance();
+        Date date= c.getTime();
+        SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        time_set = formatter.format(date);
+        record_time.setText(time_set);
 
         //生成GridView
         ArrayList<String> income_record = new ArrayList<String>();
@@ -50,10 +68,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adapter_income = new MyRecordAdapter(MainActivity.this,
                 R.layout.record_item_list,
                 income_record);
+        adapter_income.defaultTagColor();
         GridList_income.setAdapter(adapter_income);
         adapter_outcome = new MyRecordAdapter(MainActivity.this,
                 R.layout.record_item_list,
                 outcome_record);
+        adapter_outcome.defaultTagColor();
         GridList_outcome.setAdapter(adapter_outcome);
 
         //绑定监听
@@ -76,12 +96,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     //选择收入或支出
     public void income_outcome(View btn){
         if (btn.getId() == R.id.out_come){
+            GridList_outcome.setContentDescription("show");
+            GridList_income.setContentDescription("hide");
             income.setTextColor(getResources().getColor(R.color.white));
             outcome.setTextColor(getResources().getColor(R.color.gray));
             GridList_income.setVisibility(View.GONE);
             GridList_outcome.setVisibility(View.VISIBLE);
         }
         else if(btn.getId() == R.id.in_come){
+            GridList_outcome.setContentDescription("hide");
+            GridList_income.setContentDescription("show");
             outcome.setTextColor(getResources().getColor(R.color.white));
             income.setTextColor(getResources().getColor(R.color.gray));
             GridList_outcome.setVisibility(View.GONE);
@@ -91,12 +115,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-
-        adapter_income.changeSelected(position);
-        adapter_outcome.changeSelected(position);
-//        selectTag = (String)adapter_income.getItem(position);
-//        Log.i("Page", "setOnItemClickListener: tagStr:" + selectTag);
+            adapter_income.changeSelected(position);
+            adapter_outcome.changeSelected(position);
+            String selectTag_income,selectTag_outcome;
+            selectTag_outcome = (String)adapter_outcome.getItem(position);
+            selectTag_income = (String)adapter_income.getItem(position);
+            if (GridList_outcome.getContentDescription().equals("show")){
+                selectTag = selectTag_outcome;
+            }
+            else if (GridList_income.getContentDescription().equals("show")){
+                selectTag = selectTag_income;
+            }
+            Log.i("Page", "setOnItemClickListener: tagStr:" + selectTag);
 
     }
+
+    //跳转到修改时间界面
+    public void time_editor(View btn){
+        Intent time_set = new Intent(this,TimeSet.class);
+        startActivityForResult(time_set, 1);
+    }
+
+    //处理回传的时间
+    protected void onActivityResult(int requestcode, int resultCode, Intent data) {
+        if(requestcode == 1 && resultCode == 2){
+            time_set = data.getStringExtra("time_set");
+            record_time.setText(time_set);
+        }
+        super.onActivityResult(requestcode, resultCode, data);
+    }
+
 }
 
